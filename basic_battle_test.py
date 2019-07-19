7d9514c91ba39051eea117d35a40ae84531747b0
norb <noebse@gmx.de>
2019-07-19 10:55:12 +0200

    minor bugfixes and changes DOCSTRINGS for person class

--- basic_battle_test.py
+++ basic_battle_test.py
@@ -36,9 +36,11 @@ party2.add_member(fighter4)
 # party2.add_member(fighter6)
 
 if __name__ == '__main__':
-    hero.pickup_gear(battle_axe)
-    hero.pickup_gear(longsword)
-    hero.pickup_gear(short_sword)
+    # hero.pickup_gear(battle_axe)
+    # GEAR_LIST = hero.get_eqipped_items()
+    # hero.pickup_gear(longsword)
+    # hero.pickup_gear(short_sword)
+
     print('---------------------------------')
     print()
 #     # battle.whole_party_turn_battle(party1, party2)
