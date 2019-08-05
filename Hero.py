d910b2e988043cad9889f060fd194f08d36dce74
FurSofa <cml6497@gmail.com>
2019-08-05 16:05:06 -0400

    Fixed Hero.generate()!!

--- Hero.py
+++ Hero.py
@@ -2,7 +2,7 @@ import person
 
 
 class Hero(person.Person):
-    def __init__(self, name):
+    def __init__(self, name, profession):
         super().__init__(name, profession)
         self.type = 'Hero'
 
@@ -11,11 +11,13 @@ class Hero(person.Person):
         return cls(name, profession)
 
     def __str__(self):
-        return f'{super().name}'
+        return super().__str__()
 
 
-p = person.Person.generate('norb', 'mage')
+p = person.Person.generate('norbnorb', 'Mage')
+p.profession_stat_augment()
 print(p)
 
-w = Hero.generate()
+w = Hero.generate('norbnorb', 'Warrior')
+w.profession_stat_augment()
 print(w)
