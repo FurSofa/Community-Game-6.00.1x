bb08a19084ef473fd50f757a201fca17a8102202
FurSofa <cml6497@gmail.com>
2019-08-06 22:16:54 -0400

    Added TODO feature list

---
+++ TODO.py
@@ -0,0 +1,24 @@
+"""
+This file contains  TODOs and desired features list
+
+For this project we are using:
+                "Make it work then make it good"
+
+"""
+
+# Features
+
+# TODO make combat work
+
+# TODO function or class to generate an encounter or event(with or without combat)
+
+# TODO Create subClasses for all equipment(weapons with variation/ Armor for each equip spot)
+
+# TODO Refactor Class Methods and Properties
+
+# TODO Add random stats to items based on quality
+
+# TODO decide how many party members you can have and how to display them
+
+# TODO figure out how to display Equipped gear effectively
+
