bf623b176eaac7059e63ad39e7646ad55a72b86e
FurSofa <cml6497@gmail.com>
2019-08-13 14:49:50 -0400

    @Salt: doc-strings, try block logic, params all lower

--- helper_functions.py
+++ helper_functions.py
@@ -1,11 +1,11 @@
-def select_from_list(iterable, q='Whats your choice?', index_pos=False, Horizontal=False):
+def select_from_list(iterable, q='Whats your choice?', index_pos=False, horizontal=False):
     """
     Takes an iterable
     prints a numerated list for the user to choose from
     lets the user choose
     returns index position of user chosen item of iterable
     if index_pos=True will return index position of chosen object instead of object
-    :param iterable, index_pos:
+    :param iterable, index_pos, horizontal:
     :return chosen item or index position:
     """
     # Atomically choose if only 1 choice.
@@ -15,7 +15,7 @@ def select_from_list(iterable, q='Whats your choice?', index_pos=False, Horizont
         return iterable[0]
 
     print(q)
-    if Horizontal:
+    if horizontal:
         for index, item in enumerate(iterable):
             display_number = str(index + 1)
             print(display_number + ': ', item, end='  ')
@@ -29,11 +29,11 @@ def select_from_list(iterable, q='Whats your choice?', index_pos=False, Horizont
         choice = input('Number of choice: ')
         try:
             choice = 1 if not choice else int(choice)
+            if choice < 1 or choice > len(iterable):
+                print("Enter a number from the list!")
+                choice = ""
         except ValueError:
             print("Invalid choice!")
-            choice = ""
-        if choice < 1 or choice > len(iterable):
-            print("Enter a number from the list!")
             choice = ""
 
     if index_pos:
