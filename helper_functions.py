f21279b52461836f1504f7d57d6556386f483da5
salt-die <naclmolecule@gmail.com>
2019-08-13 12:47:22 -0500

    Complete refactor, combined both functions into one to avoid redundant code.

--- helper_functions.py
+++ helper_functions.py
@@ -1,4 +1,4 @@
-def select_from_list(iterable, index_pos=False, q='Whats your choice?'):
+def select_from_list(iterable, q='Whats your choice?', index_pos=False, layout="vertical"):
     """
     Takes an iterable
     prints a numerated list for the user to choose from
@@ -8,60 +8,38 @@ def select_from_list(iterable, index_pos=False, q='Whats your choice?'):
     :param iterable, index_pos:
     :return chosen item or index position:
     """
+    if len(iterable) == 1:
+        if index_pos:
+            return 0
+        return iterable[0]
+        
     print(q)
-    for index, item in enumerate(iterable):
-        display_number = str(index + 1)
-        print(display_number + ': ', item)
+    if layout == "horizontal":
+        for index, item in enumerate(iterable):
+            display_number = str(index + 1)
+            print(display_number + ': ', item, end='  ')
+    else:
+        for index, item in enumerate(iterable):
+            display_number = str(index + 1)
+            print(display_number + ': ', item)
     choice = ""
     while not choice:
+        choice = input('Number of choice: ')
         try:
-            choice = int(input('Number of choice: '))
-            if choice < 1 or choice > len(iterable):
-                print("Enter a number from the list!")
-                choice = ""
+            choice = 1 if not choice else int(choice)
         except ValueError:
             print("Invalid choice!")
-    
+            choice = ""
+        if choice < 1 or choice > len(iterable):
+            print("Enter a number from the list!")
+            choice = ""
+
     if index_pos:
         return choice - 1
-    return iterable[choice-1]
+    return iterable[choice - 1]
 
-
-def select_from_list_horizontal(iterable, index_pos=False, q='\nWho do you want to attack?'):
-    """
-    Takes an iterable
-    prints a numerated list -- HORIZONTALLY --
-    for the user to choose from, lets the user choose
-    returns index position of user chosen item of iterable
-    if index_pos=True will return index position of chosen object instead of object
-    :param question, iterable, index_pos:
-    :return chosen item or index position:
-    """
-    if len(iterable) < 2:
-        choice_index = 0
-        if index_pos:
-            return choice_index
-        return iterable[choice_index]
-    print(q)
-    for index, item in enumerate(iterable):
-        display_number = str(index + 1)
-        print(display_number + ': ', item, end='  ')
-    choice = input('')
-    if choice == '':
-        choice_index = 0
-        if index_pos:
-            return choice_index
-        return iterable[choice_index]
-
-    if not choice.isdigit() or not 0 < int(choice) <= len(iterable):
-        print('Please select a number from the list!')
-        choice_index = select_from_list(iterable, index_pos=True, q=q)
-    else:
-        choice_index = int(choice) - 1
-    if index_pos:
-        return choice_index
-    return iterable[choice_index]
-
-if __name__ == '__main__':
-    x = select_from_list(['one', 'two', 'three'])
-    y = select_from_list_horizontal(['one', 'two', 'three'], True)
+if __name__ == "__main__":
+    x = select_from_list(["Acidic Slime", "Necrotic Ooze", "Decrepit Wurm"],\
+                         "Choose a target:")
+    y = select_from_list(["Arrows", "Candles", "Lockpicks", "Not Interested"],\
+                         "What would you like to buy?", True, "horizontal")
