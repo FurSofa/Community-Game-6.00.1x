af0f459278a8961126dc1c66aa59ce2aae49536b
FurSofa <cml6497@gmail.com>
2019-08-15 19:09:49 -0400

    Started Combat system Consolidation

--- combat_funcs.py
+++ combat_funcs.py
@@ -5,7 +5,6 @@ from helper_functions import select_from_list
 def deal_multi_dmg(attacker, target_party, target_num='all', splash_dmg=25,
                    primary=True, primary_percent=100, rnd_target=True,
                    dmg_type='pysical'):
-
     if target_num == 'all' or target_num > len(target_party.members):
         target_num = len(target_party.members)
 
@@ -43,11 +42,13 @@ def deal_multi_dmg(attacker, target_party, target_num='all', splash_dmg=25,
             print('CRIT CRIT CRIT YAY!')
             dmg_dealt = dmg_dealt * dmg_mod // 100
             dmg_received_single = target.take_dmg(dmg_dealt, dmg_type)
-        print(attacker.name, 'hits', target.name, 'for', dmg_dealt, dmg_type, 'dmg and does', dmg_received_single, 'dmg')
+        print(attacker.name, 'hits', target.name, 'for', dmg_dealt, dmg_type, 'dmg and does', dmg_received_single,
+              'dmg')
         dmg_received += dmg_received_single
         members_list.remove(target)
         target_num -= 1
     return dmg_received
+
 
 # def attack(attacker,)
 # attack = {
@@ -64,3 +65,66 @@ def deal_multi_dmg(attacker, target_party, target_num='all', splash_dmg=25,
 # }
 
 
+def combat(hero_party, enemy_party, person='remove when really coding'):
+    """
+    A start to gathering functions and calculations for combat consolidation
+
+    :param hero_party: first party
+    :param enemy_party: second party
+    :param person: Not needed as a param
+    :return:
+    """
+
+    def alternating_turns(hero_party, enemy_party):
+        # Magic
+        pass
+
+    def choose_target(person, opposing_party):
+        """
+        generates a list of the targets
+        :param person:
+        :param opposing_party:
+        :return: list of targets  # this is so we can use count() later
+        """
+        # put logic for AI here
+        # put hero choice here
+        pass
+
+    def calculate_damage(person):
+        damage = person.damage
+        crit = person.is_crit
+        crit_multi = person.crit_muliplier
+        if crit:
+            damage = damage * crit_multi // 100
+
+        # if damage_type == 'Magic':
+        #     damage *= (person.int *80//100)
+        # elif damage_type == 'Physical':
+        #     damage *= (person.str * 80 // 100)
+        return damage
+
+    def defense_calc(target, damage, damage_type):
+        """
+        :param target: list
+        :return: damage
+        """
+        if len(target) > 1:
+            pass  # get the most common defense type for 'all' with count()
+        else:
+            defense = target[0].defense_type[damage_type]  # dict containing types and values
+
+        dmg_multi = damage / (damage + target[0].defense_type[damage_type])
+        actual_dmg = round(damage * dmg_multi)
+        return actual_dmg
+        pass
+
+    def attack_type_logic(not_sure_yet):
+        attack_type = ['single']
+        if attack_type == 'heal':
+            target = select_from_list(person.party)
+        elif attack_type == 'single':
+            choose_target(person='', opposing_party='')
+            # Ect
+        pass
+
+        pass
