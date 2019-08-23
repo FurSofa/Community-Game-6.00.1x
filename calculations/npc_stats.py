a8fc4db0f5598e6dfae7dd8ffc7b0f5a7e11e223
norb <noebse@gmx.de>
2019-08-23 10:39:31 +0200

    basic npc data layout suggestion

---
+++ calculations/npc_stats.py
@@ -0,0 +1,29 @@
+new_npc_stats = {
+    'base_stats': {
+        'vit': 4,
+        'dex': 4,
+        'str': 4,
+        'int': 4,
+    },
+    'derived_stats': {
+        'max_hp': 'from vit',  # vit*hp_per_vit + lvl*hp_per_lvl
+        'armor': 'from str',
+        'speed': 'from dex',
+        'dodge': 'from dex and speed',
+        'crit_chance': 'from dex',
+        'crit_dmg': 'from dex',
+    },
+    'storage': {
+        'equipped items': 'armor weapons ...',
+        'spell book': 'spells / abilities',
+        'potions': '',  # in party?
+        'party': 'party instance'
+    },
+    'tracked_values': {
+        'ct': 1000,  # when c reaches this, unit gets a turn
+        'c': 0,  # holds current charge value - +speed each clock tick in battle
+        'status_effects': [],
+        'elemental_resistance': 0,  # from items
+        'magic_resistance': 0,  # from items? int class is already too strong
+    }
+}
